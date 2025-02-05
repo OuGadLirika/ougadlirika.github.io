5322961d6854adc145cef3db8528f15852be2bea
Rudi Novak <fenevr@gmail.com>
2025-02-05 15:15:27 +0100

    done

---
+++ assets/config.js
@@ -0,0 +1,22 @@
+let config = {
+  "Title": "Roman Fenev",
+  "Name": "Roman Fenev",
+  "Description": "",
+  "links": [
+    {
+      "Title": "Telegram",
+      "URL": "https://t.me/am12admin",
+      "icon_classes": "fab fa-telegram",
+    },
+    {
+      "Title": "LinkedIn",
+      "URL": "https://www.linkedin.com/in/romanfenev/",
+      "icon_classes": "fab fa-linkedin"
+    },
+    {
+      "Title": "Resume / CV",
+      "URL": "https://drive.google.com/file/d/1lYByaZKkxGa1JPfyZAXnR3Ft60PSE3ab/view?usp=sharing",
+      "icon_classes": "fas fa-file-user"
+    },
+  ]
+}
